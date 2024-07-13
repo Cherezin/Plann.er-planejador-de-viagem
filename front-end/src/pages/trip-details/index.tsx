af190608e963dbfe567c46ba1ea491275b7793a5
Chere <alexandre2405@gmail.com>
2024-07-12 21:50:15 -0300

    Deixando a estrutura da pagina de atividade mais limpa para programar.

--- front-end/src/pages/trip-details/index.tsx
+++ front-end/src/pages/trip-details/index.tsx
@@ -62,67 +62,13 @@ export function TripDetailsPage(){
                                     <span className="text-zinc-400 text-sm ml-auto">14:00h</span>
                                 </div>
                                 <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleCheck className="size-5 text-lime-300" />
+                                    <CircleDashed className="size-5 text-zinc-400" />
                                     <span className="text-zinc-100">Cinema</span>
                                     <span className="text-zinc-400 text-sm ml-auto">18:00h</span>
                                 </div>
                             </div>
                         </div>
 
-                        {/*Dia 19*/}
-                        <div className="space-y-2.5">
-                            <div className="flex gap-2 items-baseline">
-                                <span className="text-xl text-zinc-300 font-semibold">Dia 19</span>
-                                <span className="text-xs text-zinc-500">Segunda</span>
-                            </div>
-                            <div className="space-y-2.5">
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Academia em grupo</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
-                                </div>
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Almoço</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">12:00h</span>
-                                </div>
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Gaming session</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">18:00h</span>
-                                </div>
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Jantar</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">21:00h</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/*Dia 20*/}
-                        <div className="space-y-2.5">
-                            <div className="flex gap-2 items-baseline">
-                                <span className="text-xl text-zinc-300 font-semibold">Dia 20</span>
-                                <span className="text-xs text-zinc-500">Terça</span>
-                            </div>
-                            <div className="space-y-2.5">
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Academia em grupo</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
-                                </div>
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleDashed className="size-5 text-zinc-400" />
-                                    <span className="text-zinc-100">Almoço</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">12:00h</span>
-                                </div>
-                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                                    <CircleDashed className="size-5 text-zinc-400" />
-                                    <span className="text-zinc-100">Jantar</span>
-                                    <span className="text-zinc-400 text-sm ml-auto">21:00h</span>
-                                </div>
-                            </div>
-                        </div>
 
                     </div>
                 </div>
