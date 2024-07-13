ccb9d07e72d4f40219be91c5118896c8c27bb041
Chere <alexandre2405@gmail.com>
2024-07-13 16:57:19 -0300

    Alterando as divs de modal pelo componente modal

--- front-end/src/pages/trip-details/create-activity-modal.tsx
+++ front-end/src/pages/trip-details/create-activity-modal.tsx
@@ -1,6 +1,7 @@
 import { Calendar, Tag } from "lucide-react";
 import { RefObject } from "react";
 import { Button } from "../../components/button";
+import { Modal } from "../../components/modal";
 
 interface CreateActivityModalProps{
     modalRef: RefObject<HTMLDivElement>
@@ -9,25 +10,25 @@ interface CreateActivityModalProps{
 export function CreateActivityModal(props: CreateActivityModalProps){
     return(
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-        <div ref={props.modalRef} className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <Modal ref={props.modalRef}>
                 
-            {/* Cabeçalho do Modal */}
-            <div className='space-y-2'>
-                <div className='flex items-center justify-between'>
-                    <h2 className='text-lg font-semibold'>Cadastrar atividade</h2>
+                {/* Cabeçalho do Modal */}
+                <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className='text-lg font-semibold'>Cadastrar atividade</h2>
                     </div>
                     <p className='text-sm text-zinc-400'>
-                    Todos convidados podem vizualizar as atividades.
+                        Todos convidados podem vizualizar as atividades.
                     </p>
                 </div>
-    
+        
                 <form  className='space-y-3'>
                     <div className='h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2'>
-                    <Tag className='size-5 text-zinc-400'/>
-                    <input 
-                    name='title' 
-                    placeholder="Qual a atividade?" 
-                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+                        <Tag className='size-5 text-zinc-400'/>
+                        <input 
+                        name='title' 
+                        placeholder="Qual a atividade?" 
+                        className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
                     </div>
 
                     <div className="flex items-center gap-2">
@@ -41,11 +42,11 @@ export function CreateActivityModal(props: CreateActivityModalProps){
                         </div>
                     </div>
 
-                <Button type="submit" size="full">
-                    Salvar atividade
-                </Button>
+                    <Button type="submit" size="full">
+                        Salvar atividade
+                    </Button>
                 </form>
-            </div>
+            </Modal>
         </div>
     )
 }
