e3a4e3bc62aa9d2906ab259cc82a1be2a42cb6c9
Chere <alexandre2405@gmail.com>
2024-07-13 16:42:28 -0300

    Criando nosso componente modal

---
+++ front-end/src/components/modal.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps, ReactNode, forwardRef } from "react";
+import { tv } from "tailwind-variants";
+
+const modalVariants = tv({
+    base: 'w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'
+});
+
+interface ModalProps extends ComponentProps<'div'> {
+    children: ReactNode;
+}
+
+export const Modal = forwardRef<HTMLDivElement, ModalProps>(({ children, ...props }, ref) => {
+    return (
+        <div {...props} ref={ref} className={modalVariants()}>
+            {children}
+        </div>
+    );
+});
